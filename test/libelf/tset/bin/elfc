chimera-linux/elftoolchain

commit 5189c2fbf165db3cacb61eec659d0b1b702b7508

Use modern Python constructs.
Ticket: [#547]

git-svn-id: https://svn.code.sf.net/p/elftoolchain/code/trunk@3864 95820547-d848-0410-985e-9ae8fe0fa350

test/libelf/tset/bin/elfc
@@ -84,7 +84,7 @@ usage = "usage: %prog [options] [input-file]"
 description = """Create an ELF binary from a textual description in """ + \
  """'input-file' (or stdin)"""
 
-import optparse, re, struct, sys, types, yaml
+import io, operator, optparse, re, struct, sys, types, yaml
 
 
 class ElfError(Exception):
@@ -110,13 +110,13 @@ class ElfError(Exception):
 
         if self.ee_start != -1:
             if self.ee_start == self.ee_end:
-                return "Error: line %d: %s" % (self.ee_start, self.ee_msg)
+                return "Error: line {start}: {msg}".format(
+                    start=self.ee_start, end=self.ee_msg)
             else:
-                return "Error: lines %d--%d: %s" % \
-                       (self.ee_start, self.ee_end,
-                 self.ee_msg)
+                return "Error: lines {start}--{end}: {msg}".format(
+                    start=self.ee_start, end=self.ee_end, msg=self.ee_msg)
         else:
-            return "Error: %s" % self.ee_msg
+            return "Error: {msg}".format(msg=self.ee_msg)
 
 
 #
@@ -571,12 +571,12 @@ def encode_flags(flags, m):
 def check_dict(d, l, node=None):
     """Check a dictionary for unknown keys."""
     unknown = []
-    for k in d.keys():
+    for k in d.iterkeys():
         if k not in l:
             unknown.append(k)
     if len(unknown) > 0:
-        raise ElfError(node, "{%s} Unknown key(s) %s" % \
-                (node.tag, unknown))
+        raise ElfError(node, "{tag} Unknown key(s) {key}".format(
+            tag=node.tag, key=unknown))
 
 
 def bounded_value(v, encoding):
@@ -623,10 +623,9 @@ class ElfStrTab:
     def bits(self):
         """Return the contents of an ELF string table."""
 
-        l = self.htab.items()
-        l.sort(lambda x, y: cmp(x[1], y[1]))  # Order by string offset.
+        # Prepare the string table, ordered by string offset.
         ls = [""]  # initial NUL
-        for (ss, oo) in l:
+        for (ss, oo) in sorted(self.htab.items(), key=operator.itemgetter(1)):
             ls.append(ss)
         return "\000".join(ls) + "\000"  # Add trailing NULs
 
@@ -672,9 +671,9 @@ class ElfType:
                 v = fn(d, name)
                 setattr(self, f[0], v)
             except:
-                raise ElfError(node,
-                        'key: "%s" value: "%s" unrecognized.' % \
-                      (name, d[name]))
+                raise ElfError(
+                    node, "key: {key!r} value: {value!r} unrecognized.".format(
+                        key=name, value=[name]))
         self._n = node  # Save YAML node and associated value
         self._d = d  # for error reporting.
 
@@ -749,7 +748,7 @@ class ElfType:
             elif f == "":
                 pass
             else:
-                raise TypeError, "Invalid format char '%s'." % f
+                raise TypeError("Invalid format char {char!r}.".format(char=f))
         return sz
 
 
@@ -1233,7 +1232,8 @@ class ElfSection(ElfType):
 
         if self._strtab:
             return self._strtab.lookup(name)
-        raise ElfError(None, 'Cannot translate "%s" to an index.' % name)
+        raise ElfError(
+            None, "Cannot translate {name!r} to an index.".format(name=name))
 
     def bits(self, formatchar, elfclass):
         raise AssertionError, "Section objects should use " \
@@ -1251,9 +1251,10 @@ class ElfSection(ElfType):
                     self.sh_name = \
                         elf.section_name_index(self.sh_name)
                 except KeyError:
-                    raise ElfError(self._n,
-                          "Section name '%s' not in string table." % \
-                          self.sh_name)
+                    raise ElfError(
+                        self._n,
+                        "Section name {name!r} not in string table.".format(
+                            name=self.sh_name))
         # give a chance for the contents of a section to xlate strings
         for d in self.sh_data:
             if isinstance(d, ElfType):
@@ -1355,7 +1356,7 @@ class ElfSym(ElfType):
                   elf.elf_sections.get_index(self.st_shndx)
             if self.st_shndx is None:
                 raise ElfError(self._n, "Untranslateable 'st_shndx' " + \
-                 "value \"%s\"." % self.st_shndx)
+                 "value {value!r}.".format(value=self.st_shndx))
 
         if type(self.st_name) == types.StringType:
             try:
@@ -1369,7 +1370,9 @@ class ElfSym(ElfType):
             try:
                 self.st_name = strtab.lookup(self.st_name)
             except KeyError:
-                raise ElfError(self._n, 'unknown string "%s"' % self.st_name)
+                raise ElfError(
+                    self._n,
+                    "unknown string {string!r}".format(string=self.st_name))
         return offset
 
 
@@ -1541,7 +1544,7 @@ class ElfSectionList:
             for sh in self.shl_sections:
                 if sh.sh_index == ind:
                     return sh
-            raise IndexError, "no section at index %d" % ind
+            raise IndexError("no section at index {index}".format(index=ind))
 
     def layout(self, offset, elf):
         """Compute the layout for section."""
@@ -1672,7 +1675,7 @@ class Elf:
         if offset < pos or (offset > pos and self.elf_fillchar == 0):
             f.seek(offset, 0)
             return
-        s = ("%c" % self.elf_fillchar) * (offset - pos)
+        s = ("{fillchar}".format(fillchar=self.elf_fillchar) * (offset - pos))
         f.write(s)
 
 
@@ -1763,11 +1766,11 @@ if __name__ == '__main__':
 
     try:
         if args:
-            stream = file(args[0], 'r')
+            stream = io.open(args[0], 'r')
         else:
             stream = sys.stdin
-    except IOError, x:
-        parser.error("cannot open stream: %s" % x)
+    except IOError as err:
+        parser.error("cannot open stream: {err}".format(err=err))
 
     init_parser()
 
@@ -1775,9 +1778,9 @@ if __name__ == '__main__':
         elf = make_elf(yaml.load(stream, Loader=yaml.Loader))
         elf.layout()
         elf.write(options.output)
-    except yaml.YAMLError, x:
-        parser.error("cannot parse stream: %s" % x)
-    except ElfError, msg:
+    except yaml.YAMLError as err:
+        parser.error("cannot parse stream: {err}".format(err=err))
+    except ElfError as msg:
         print msg
         sys.exit(1)
 

commit 1984106c14c13b9008e15c150e463fb4405da55f

Introduce a 'sh_word_size' pseudo field that allows the file size of integer literals to be specified in a YAML description.
Modernize a few more Python constructs.

Correct a typo introduced in [r3864].

Ticket: [#547]

git-svn-id: https://svn.code.sf.net/p/elftoolchain/code/trunk@3865 95820547-d848-0410-985e-9ae8fe0fa350

test/libelf/tset/bin/elfc
@@ -488,6 +488,7 @@ defaults = {
     'sh_offset': None,
     'sh_size': None,
     'sh_type': 'SHT_NULL',
+    'sh_word_size': 32,
     # Verdaux
     'vda_name': 0,
     'vda_next': 0,
@@ -673,7 +674,7 @@ class ElfType:
             except:
                 raise ElfError(
                     node, "key: {key!r} value: {value!r} unrecognized.".format(
-                        key=name, value=[name]))
+                        key=name, value=d[name]))
         self._n = node  # Save YAML node and associated value
         self._d = d  # for error reporting.
 
@@ -950,11 +951,14 @@ class ElfEhdr(ElfType):
               type(self.e_shstrndx) == types.LongType:
                 pass
             else:
-                raise ElfError(self._n, "Unparseable e_shstrndx field.")
+                raise ElfError(
+                    self._n,
+                    "Unparseable e_shstrndx field: {v!r}".format(
+                        v=self.e_shstrndx))
             if self.e_shstrndx is None:
                 raise ElfError(self._n,
                         'Cannot determine section ' + \
-                        'name string table index.')
+                        'name string table index: {v!r}'.format(v=self.e_shstrndx))
         else:
             if self.e_shstrndx is None:
                 self.e_shstrndx = 0
@@ -1169,11 +1173,14 @@ class ElfSection(ElfType):
     YAML tag: !Section
 
     A section description consists of the fields that make up an ELF
-    section header entry and an additional field 'sh_data' that
-    contains the data associated with this section.
+    section header entry with the following additional fields:
 
-    'sh_data' may be a YAML string, or a YAML list of items that
-    comprise the content of the section.
+    - Field 'sh_data' contains the data associated with this section.
+      'sh_data' may be a YAML string, or can be a YAML list of items
+      that comprise the content of the section.
+    - Field 'sh_index' can be used to manually set the section's index.
+    - Field 'sh_word_size' specifies the word size to use for data in
+      integer literal form.  The value of this field can be 32 or 64.
     """
 
     fields = [
@@ -1189,7 +1196,8 @@ class ElfSection(ElfType):
         ('sh_addralign', do_copy, "I", "Q"),
         ('sh_entsize', do_long, "I", "Q"),
         ('sh_data', do_copy, "", ""),
-        ('sh_index', do_long, "", "")
+        ('sh_index', do_long, "", ""),
+        ('sh_word_size', do_long, "", ""),
     ]
 
     def __init__(self, shdr, node):
@@ -1207,6 +1215,11 @@ class ElfSection(ElfType):
             if (self.sh_addralign == 0 or \
              (self.sh_addralign & (self.sh_addralign - 1)) != 0):
                 raise ElfError(node, "'sh_addralign' not a power of two.")
+        if self.sh_word_size != 32 and self.sh_word_size != 64:
+            raise ElfError(
+                node,
+                "unsupported 'sh_word_size' value: {v!r}".format(
+                    v=self.sh_word_size))
         self._data = None  # 'cache' of translated data
         self._strtab = None
 
@@ -1236,8 +1249,8 @@ class ElfSection(ElfType):
             None, "Cannot translate {name!r} to an index.".format(name=name))
 
     def bits(self, formatchar, elfclass):
-        raise AssertionError, "Section objects should use " \
-              "databits() or headerbits()"
+        raise AssertionError("Section objects should use " \
+                             "databits() or headerbits()")
 
     def layout(self, offset, elf):
         """Prepare an ELF section for output."""
@@ -1298,10 +1311,13 @@ class ElfSection(ElfType):
         for d in self.sh_data:
             if isinstance(d, ElfType):
                 s += d.bits(formatchar, elfclass)
+            elif isinstance(d, types.IntType):
+                word_size_char = "I"
+                if self.sh_word_size == 64:
+                    word_size_char = "Q"
+                s += struct.pack(formatchar + word_size_char, d)
             elif isinstance(d, types.LongType):
                 s += struct.pack(formatchar + "Q", d)
-            elif isinstance(d, types.IntType):
-                s += struct.pack(formatchar + "I", d)
             else:
                 s += str(d)
         return s
@@ -1634,7 +1650,7 @@ class Elf:
 
         Argument 'fn' denotes the destination."""
 
-        of = file(fn, 'w')
+        of = io.open(fn, 'wb')
 
         formatchar = self.formatchar()
         elfclass = self.elfclass()